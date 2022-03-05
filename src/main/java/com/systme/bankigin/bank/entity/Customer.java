b7326aa7dd48484c6fe0a6b285a3e36c5c73e934
alijavaboy <ali77erfagh99@gmail.com>
2022-03-05 13:04:43 +0330

    add customer and employee

---
+++ src/main/java/com/systme/bankigin/bank/entity/Customer.java
@@ -0,0 +1,9 @@
+package com.systme.bankigin.bank.entity;
+
+import com.systme.bankigin.bank.entity.profile.Person;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Customer extends Person {
+}
---
+++ src/main/java/com/systme/bankigin/bank/entity/Employee.java
@@ -0,0 +1,10 @@
+package com.systme.bankigin.bank.entity;
+
+import com.systme.bankigin.bank.entity.profile.Person;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Employee extends Person {
+
+}
