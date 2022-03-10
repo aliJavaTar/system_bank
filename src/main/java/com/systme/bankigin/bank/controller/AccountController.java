a6a134739d8893b86bfb89fe6dd0a6dd6893ef3e
alijavaboy <ali77erfagh99@gmail.com>
2022-03-10 12:47:59 +0330

    add TransactionController

--- src/main/java/com/systme/bankigin/bank/controller/AccountController.java
+++ src/main/java/com/systme/bankigin/bank/controller/AccountController.java
@@ -2,10 +2,7 @@ package com.systme.bankigin.bank.controller;
 
 import com.systme.bankigin.bank.entity.Account;
 import com.systme.bankigin.bank.service.AccountService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AccountController {
---
+++ src/main/java/com/systme/bankigin/bank/controller/TransactionController.java
@@ -0,0 +1,7 @@
+package com.systme.bankigin.bank.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TransactionController {
+}
